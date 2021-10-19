1496f922c56de666a0ebcc2b7dc0da509464e112
je0489 <je0489@naver.com>
2021-10-19 22:10:56 +0900

    Docs: Add graph search algorithms for BFS and DFS

--- documents/.vuepress/const.js
+++ documents/.vuepress/const.js
@@ -1,3 +1,4 @@
 exports.jsList = ["javascript/syntax.md"];
 exports.vueList = ["vuepress/started_vue.md", "vuepress/custom_theme.md"];
+exports.algorithmList = ["algorithm/graph_search.md"];
 exports.etcList = ["etc/markdown.md"];
